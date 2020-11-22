1dd52773a93f64168e1c928b0469b6f5d0e87078
Enes Karaosman <eneskaraosman53@gmail.com>
2020-11-22 07:57:39 +0300

    [MOD] PuzzleElement now conforms to `Identifiable` protocol

--- Sources/PuzzleMaker/PuzzleElement.swift
+++ Sources/PuzzleMaker/PuzzleElement.swift
@@ -11,9 +11,18 @@ import UIKit
 /**
  *  Describes single puzzle element, which holds prepared image, position on the board and puzzle unit which provides detailed information about path
  */
-public struct PuzzleElement {
+public struct PuzzleElement: Identifiable {
 
     // MARK: Properties
+    
+    public var id: String {
+        "\(elementPosition.0).\(elementPosition.1)"
+    }
+    
+    /// UIView calculates position based on topLeft edge, so convert how SwiftUI position works
+    public var centerPosition: CGPoint {
+        CGPoint(x: position.x + image.size.width / 2, y: position.y + image.size.height / 2)
+    }
 
     /// Cropped and clipped image with additional effects: two inner shadows (light and dark to make it "more real")
     public let image: UIImage
